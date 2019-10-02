c322c408526811ecb281a9d016cec6efe9f43b2d
student 04 <stk_lum_04@outlook.de>
2019-10-02 15:31:07 +0200

    BMI

--- Program.cs
+++ Program.cs
@@ -10,6 +10,49 @@ namespace BMI
     {
         static void Main(string[] args)
         {
+            double BMI, Größe, Masse;
+
+            Console.WriteLine("Aus Größe und Masse soll der BMI berechnet werden");
+            Console.WriteLine("Masse in kg angeben:");
+            Masse = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Größe in m angeben:");
+            Größe = Convert.ToDouble(Console.ReadLine());
+
+            BMI = Masse / Math.Pow(Größe,2);
+            BMI = Math.Round(BMI, 1);
+
+            if (BMI < 16)
+            {
+                Console.WriteLine("Der BMI beträgt {0}, das bedeutet Starkes Untergewicht", BMI);
+            }
+            else if (BMI <= 17)
+            {
+                Console.WriteLine("Der BMI beträgt {0}, das bedeutet Mäßiges Untergewicht", BMI);
+            }
+            else if (BMI <= 18.5)
+            {
+                Console.WriteLine("Der BMI beträgt {0}, das bedeutet Leichtes Untergewicht", BMI);
+            }
+            else if (BMI <= 25)
+            {
+                Console.WriteLine("Der BMI beträgt {0}, das bedeutet Normalgewicht", BMI);
+            }
+            else if (BMI <= 30)
+            {
+                Console.WriteLine("Der BMI beträgt {0}, das bedeutet Präadipositas", BMI);
+            }
+            else if (BMI <= 35)
+            {
+                Console.WriteLine("Der BMI beträgt {0}, das bedeutet Adipositas Grad I ", BMI);
+            }
+            else if (BMI < 40)
+            {
+                Console.WriteLine("Der BMI beträgt {0}, das bedeutet Adipositas Grad II ", BMI);
+            }
+            else
+            {
+                Console.WriteLine("Der BMI beträgt {0}, das bedeutet Adipositas Grad III ", BMI);
+            }
         }
     }
 }
